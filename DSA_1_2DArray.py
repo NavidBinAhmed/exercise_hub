b8b9d1ed793dcd45b99c7ad2f6608b25c11324a5
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2024-12-18 02:51:52 +0600

    updated with some theories on sorting and insertion-deletion applications of array

--- DSA_1_2DArray.py
+++ DSA_1_2DArray.py
@@ -12,3 +12,14 @@ print("member at 1x2 index is", mem2)
 
 mem3 = mat[2][2]
 print("member at 3x3 index is", mem3)
+
+
+#application of array 1# insertion and deletion
+'''linked list is preferable instead of array due to its
+constant space complexity instead of O(n)'''
+#application of array 2# sorting- ascending and descending
+'''concept of sorting algorithm
+1. in-place sorting- no extra space/ no external data structure is used
+2. out-place sorting- extra space/ external data structure is used (merge sort)'''
+
+
