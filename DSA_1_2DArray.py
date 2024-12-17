45d08ebf9873e1644fccc559a0206f8f2ed91595
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2024-12-18 03:12:55 +0600

    updated with a leetcode problem no 75

--- DSA_1_2DArray.py
+++ DSA_1_2DArray.py
@@ -23,3 +23,40 @@ constant space complexity instead of O(n)'''
 2. out-place sorting- extra space/ external data structure is used (merge sort)'''
 
 
+#leetcode 75 - sort colors
+from typing import List
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        p0 = 0
+        curr = 0
+        p3 = 0
+        #p4 = 0
+        p2 = n - 1
+
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[curr], nums[p0] = nums[p0], nums[curr]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1
+            elif nums[curr] == 3:
+                nums[curr], nums[p3] = nums[p3], nums[curr]
+                p3 -= 1
+            #elif nums[curr] == 4:
+            #    nums[curr], nums[p4] = nums[p4], nums[curr]
+            #    p4 += 1
+            else:
+                curr += 1
+
+# Example Usage:
+nums = [2, 3, 0, 2, 1, 3, 1, 0, 3]
+solution = Solution()
+solution.sortColors(nums)
+print(nums)  # Output should be [0, 0, 1, 1, 2, 2]
