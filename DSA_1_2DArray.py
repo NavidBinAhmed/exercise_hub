8d4352641a45c1c3b88cfc626a0a478dc462820a
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2024-12-17 21:21:41 +0600

    matrix applications

---
+++ DSA_1_2DArray.py
@@ -0,0 +1,14 @@
+#2D array - Matrix computation
+
+mat = [[1,2,3], 
+       [4,5,6],
+       [7,8,9]]
+
+mem1 = mat[0][0]
+print("member at 0x0 index is", mem1)
+
+mem2 = mat[1][2]
+print("member at 1x2 index is", mem2)
+
+mem3 = mat[2][2]
+print("member at 3x3 index is", mem3)
