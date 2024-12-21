0d2d8a5c94cbf0e1cc132ce9ffdc649c2ee24802
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2024-12-22 00:10:35 +0600

    updated with common ds

--- DSA_1_list_tup_set_dict.py
+++ DSA_1_list_tup_set_dict.py
@@ -16,21 +16,22 @@ print(tpl1)
 '''sets do not use duplicates, lists do'''
 '''uses curly braces {}'''
 '''index number is unique for a particluar element'''
+'''can not call element using the index number (like tuple, list or dict)'''
+'''set1[0] in below code - showing TypeError''' 
+
 set1 = {1,2,3,2,4}
-print(set1) 
+print(set1)   
 
-
+# dictionary
 '''dictionary uses curly braces, and key:value pair'''
 '''key is unique not allowing duplicates like a sets' element'''
-
-dict1 ={'key': 'value', 'nationality': ''}
-print(dict1)
-print(type(dict1))
-
 '''we are entering the manual index number by ourselves as the key (name)'''
 '''in set index number is the key'''
 '''common thing is both uses hashmaps'''
 '''dictionary is mutable, as we see below BD is replaced by USA'''
+dict1 ={'key': 'value', 'nationality': ''}
+print(dict1)
+print(type(dict1))
 
 dictionary = {'name': 'navid', 'place': 'BD', 'email': 'navid@yahoo.com'}
 print(dictionary)
@@ -38,3 +39,6 @@ print(dictionary['email'])
 print(dictionary['place'])
 dictionary['place'] = 'USA'
 print(dictionary)
+
+
+# string
