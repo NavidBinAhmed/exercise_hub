5113b3b46996f6f81fe8378095a9f1d25689f661
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2024-12-22 00:31:46 +0600

    updated with some examples of string

--- DSA_1_list_tup_set_dict.py
+++ DSA_1_list_tup_set_dict.py
@@ -42,3 +42,47 @@ print(dictionary)
 
 
 # string
+'''string is mutable'''
+'''accessible using indexing- only sets can't be accessed using index numbers'''
+'''uses inverted: "" or '' to express'''
+'''below are some operations in string'''
+my_string = 'I am Navid'
+print(my_string)
+
+first_char = my_string[0]
+second_char = my_string[-1]
+print(first_char)
+print(second_char)
+
+substring = my_string[0:4] # outputs: I am
+print(substring)
+print(len(my_string))
+
+greeting = "Hello!"
+full_string = greeting + ' ' + my_string + '.'
+print(full_string)
+
+my_s_u = my_string.upper()
+my_s_l = my_string.lower()
+print(my_s_u)
+print(my_s_l)
+
+replaced_string = my_string.replace("I am", "He is")
+print(replaced_string)
+
+index_finding = my_string.find('Navid')
+print(index_finding)
+
+new_string = 'Hello, I am Navid, He is Akash.'
+words = new_string.split(',')
+print(words)
+joined_string = '-'.join(words)
+print(joined_string)
+
+contained_word = "Akash" in new_string
+print(contained_word)
+
+string = "Navid"
+for char in string:
+    print(char, end = ',')
+print() # for a new line
