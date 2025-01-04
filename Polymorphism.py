461fc8cf6aa691e35bd076e25ac2cbc82dd33821
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2025-01-04 09:18:25 +0600

    more code on OOPs

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,6 +1,3 @@
-'''examples are from Krish Naik uDemy course on GenAI'''
-
-
 # Polymorphism
 class Animal:
     def speak(self):
@@ -29,7 +26,6 @@ print(cat.speak())
 print(Animal().speak())
 
 animal_speak(cat)
-
 
 # polymorphism with functions and methods
 # base class
@@ -64,34 +60,3 @@ circle = Circle(3)
 
 print_area(rectangle)
 print_area(circle)
-
-
-# Polymorphism with abstract base class
-from abc import ABC, abstractmethod
-
-# define an abstract class: complete empty class
-class Vehicle(ABC):
-    @abstractmethod
-    def start_engine(self):
-        pass
-
-# derived class 1
-class Car(Vehicle):
-    def start_engine(self):
-        return "Car engine started"
-    
-# derived class 2
-class Motorcycle(Vehicle):
-    def start_engine(self):
-        return "Motorcycle engine started"
-    
-
-# function demostrating polumorphism
-def start_vehicle(vehicle):
-    print(vehicle.start_engine())
-
-# create objects of car and motorcycle
-car = Car()
-motorcycle = Motorcycle()
-start_vehicle(car)
-start_vehicle(motorcycle)
