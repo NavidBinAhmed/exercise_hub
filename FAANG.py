7da5621f7ee0e19a6e44b5d9a3f5f9ba3a49c5ca
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2025-01-10 15:50:17 +0600

    updated qa

--- FAANG.py
+++ FAANG.py
@@ -38,3 +38,54 @@ target = float('inf')
 for i in range(len(arr_2)-1):
   if arr_2[i] == target:
     print(i)
+
+
+'''2. Check the presence of element in 2D array'''
+def searchMatrix(matrix, target):
+
+# count rows
+  m = len(matrix)
+  if m == 0:
+    return False
+  
+  # count columns
+  n = len(matrix[0])
+  
+  left, right = 0, m*n - 1
+  while left <= right:
+    mid = left + (right - left)//2
+    mid_element = matrix[mid//n][mid%n]
+    if target == mid_element:
+      return True
+    elif target < mid_element:
+      right = mid - 1
+    else:
+      left = mid + 1
+  return False
+
+# driver code
+matrix = [[1,2,3],
+          [5,6,7],
+          [9,10,11]]
+target = 11
+# function calling
+result = searchMatrix(matrix, target)
+print(result)
+
+
+'''brute force approach - linear'''
+def searcharray(matrix, target):
+
+  for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+      if matrix[i][j] == target:
+        return True
+  return False
+
+# driver code
+matrix = [[1,2,3,4],
+          [5,6,7,8,],
+          [9,10,11,12]]
+target = 7
+result = searcharray(matrix, target)
+print(result)
